5e6b7503293e32e961534b20d03110a0b2fa6c56
Pritam Mondal <pritammondal2825@gmail.com>
2026-07-27 20:36:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ MOVIE_UPDATE_CHANNEL = [int(x) for x in environ.get('MOVIE_UPDATE_CHANNEL', '-10
 DREAMXBOTZ_IMAGE_FETCH = bool(environ.get('DREAMXBOTZ_IMAGE_FETCH', True))  # On (True) / Off (False)
 LINK_PREVIEW = bool(environ.get('LINK_PREVIEW', False)) # Shows link preview in notification msg instead of image
 ABOVE_PREVIEW = bool(environ.get('ABOVE_PREVIEW', True)) # Shows link preview above the text in notification msg if True else below the msg
-TMDB_API_KEY = environ.get('TMDB_API_KEY', 'a802a46fb5c3f909c408f885b343117f) # preffer to use your own tmdb API Key get it from https://www.themoviedb.org/settings/api
+TMDB_API_KEY = environ.get('TMDB_API_KEY', 'a802a46fb5c3f909c408f885b343117f') # preffer to use your own tmdb API Key get it from https://www.themoviedb.org/settings/api
 TMDB_POSTER = bool(environ.get('TMDB_POSTER', True)) # Shows TMDB poster in notification msg
 LANDSCAPE_POSTER = bool(environ.get('LANDSCAPE_POSTER', True)) # Shows landscape poster in notification msg
 
