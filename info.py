df210b4c01a42f10f20e9f455d0b162e3d044e0c
Pritam Mondal <pritammondal2825@gmail.com>
2026-07-27 15:19:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,13 +107,13 @@ TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/Pixell_Pulse/26")   # Secon
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/Pixell_Pulse/27")   # Third tutorial link for verification
 
 # Verification (Must Fill All Veriables. Else You Got Error
-SHORTENER_API = environ.get("SHORTENER_API", "4e011baa509ef8f4d20121855627c368c42e927a" ) # Shortener API key
+SHORTENER_API = environ.get("SHORTENER_API", "4e011baa509ef8f4d20121855627c368c42e927a") # Shortener API key
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "papajiurl.com") # Shortener website
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "4e011baa509ef8f4d20121855627c368c42e927a" )  # Shortener API key for second website
+SHORTENER_API2 = environ.get("SHORTENER_API2", "4e011baa509ef8f4d20121855627c368c42e927a")  # Shortener API key for second website
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "papajiurl.com") # Shortener website for second website
 
-SHORTENER_API3 = environ.get("SHORTENER_API3", "4e011baa509ef8f4d20121855627c368c42e927a" )  
+SHORTENER_API3 = environ.get("SHORTENER_API3", "4e011baa509ef8f4d20121855627c368c42e927a")  
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "papajiurl.com") # Shortener website for third website
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "43200")) # Time gap for two-step verification in seconds (default: 20 minutes)
